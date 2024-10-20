22a98444a02336d49b8a316f44ea35f28bc779c9
dedioficial <69488179+dedioficial@users.noreply.github.com>
2024-10-20 20:04:02 -0300

    fix(navbar): position

--- src/components/ui/Navbar/index.tsx
+++ src/components/ui/Navbar/index.tsx
@@ -5,7 +5,7 @@ import { Navlist } from "./Navlist";
 export const Navbar = () => {
   return (
     <>
-      <header className="flex gap-10 fixed top-0 left-0 right-0 w-full px-20 max-[700px]:px-6 py-6 items-center z-50">
+      <header className="flex gap-10 absolute top-0 left-0 right-0 w-full px-20 max-[700px]:px-6 py-6 items-center z-50">
         <Image
           src="/images/logo/logotype.svg"
           alt="Sonner logotype"
